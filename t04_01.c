2ffff95d4f904a56f6918ad95699577a5a20c8e7
ImmanuelTambunan <immannueltambunan@gmail.com>
2025-02-27 14:41:50 +0700

    Pair 27 Done

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,24 @@
-// NIM - Name
-// NIM - Name
-
+// 12S24034 - Immanuel Alexander Tambunan
+// 12S24046 - Indah Elsadai Nainggolan
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int _argv, char **_argc) {
-  // codes
-
-  return 0;
-}
+int main(int argc, char *argv[]) {
+ 
+      char str[61]; 
+      scanf("%60s", str); 
+      
+      int length = strlen(str);
+      for (int i = 0; i < length; i += 3) {
+          char segment[4]; 
+          strncpy(segment, str + i, 3);
+          segment[3] = '\0'; 
+          
+          int ascii_value = atoi(segment); 
+          printf("%c", ascii_value); 
+      }
+      
+      printf("\n"); 
+      return 0;
+  }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,24 @@
-// NIM - Name
-// NIM - Name
-
+// 12S24034 - Immanuel Alexander Tambunan
+// 12S24046 - Indah Elsadai Nainggolan
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+int main() {
+  char str[21];
+  int i;
 
+  fgets(str, sizeof(str), stdin);
+
+  if (str[strlen(str) - 1] == '\n') {
+    str[strlen(str) - 1] = '\0';
+  }
+
+  for (i = 0; str[i] != '\0'; i++) {
+      printf("%03d", str[i]);
+  }
+  printf("013");
   return 0;
 }
+
+  
